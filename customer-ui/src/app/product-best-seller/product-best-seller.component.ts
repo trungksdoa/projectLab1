6961335299aec9bc445970979067a463061dc7f7
nguyencaothai <lanhoxga@gmail.com>
2022-06-18 21:25:36 +0700

    fix

--- customer-ui/src/app/product-best-seller/product-best-seller.component.ts
+++ customer-ui/src/app/product-best-seller/product-best-seller.component.ts
@@ -19,7 +19,39 @@ export class ProductBestSellerComponent implements OnInit {
     this.router.navigate([`detail/${id}`])
   }
 
-  slideConfig = { slidesToShow: 4, slidesToScroll: 4 }
+  slideConfig = {
+    "slidesToShow": 4,
+    "slidesToScroll": 1,
+    "dots": true,
+    "infinite": false,
+    "responsive": [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+      
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        margin:100,
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
   slickInit (e) {
     console.log('slick initialized')
